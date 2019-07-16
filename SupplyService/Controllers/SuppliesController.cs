5eabcc9b92642e82c9737c800cb6cc72c78ef7f9
namelessking <habibur.tanjin@gmail.com>
2019-07-16 17:59:46 +0200

    added method to save ProductData

--- SupplyService/Controllers/SuppliesController.cs
+++ SupplyService/Controllers/SuppliesController.cs
@@ -6,11 +6,25 @@ using Common;
 using Microsoft.AspNetCore.Mvc;
 using OrderService.Repositories;
 using SupplyService.Model;
+using MongoDB.Bson;
+using Microsoft.AspNetCore.Http;
 
 namespace SupplyService.Controllers
 {
     public class SuppliesController : ControllerBase
     {
+
+
+        private  IHttpContextAccessor httpContextAccessor;
+        public void IPAddressController(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+
+
+
+
         [HttpPost]
         public IActionResult GetProductBySKU(string sku)
         {
@@ -33,5 +47,39 @@ namespace SupplyService.Controllers
 
             return new JsonResult(new SuccessWithResult<IEnumerable<ProductData>>(products));
         }
+
+
+        [HttpPost]
+        public IActionResult Save([FromBody] ProductData data)
+        {
+            if (data.Availability<=0)
+                return new JsonResult(new Fault("Product is non available"));
+
+            var productData = new ProductData
+            {
+
+                Description = data.Description,
+                Availability = data.Availability,
+                Categories = data.Categories
+                //Id = ObjectId.GenerateNewId(),
+                //Address = data.Address,
+                //Items = from item in data.Items
+                //        select new OrderItem { SKU = new ObjectId(item.Key), Quantity = item.Value }
+            };
+
+            SuppliesRepository repository = new SuppliesRepository(Program.connectionString);
+
+            repository.SaveProduct(productData);
+
+
+            //var ip = this.;
+            return new JsonResult(new SuccessWithResult<string>(productData.Id.ToString()));
+            //return new JsonResult(new SuccessWithResult<string>(new ProductResponse().URI=));
+        }
+
+
+
+
+
     }
 }
---
+++ SupplyService/Model/ProductResponse.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupplyService.Model
+{
+    public class ProductResponse
+    {
+        
+        public string URI { get; set; }
+
+        public ObjectId Id { get; set; }
+      
+        public string Description { get; set; }
+    
+        public double Availability { get; set; }
+
+        public IEnumerable<string> Categories { get; set; }
+    }
+}
